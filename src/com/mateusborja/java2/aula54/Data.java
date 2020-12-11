68dc34d24b37ec1e4635f00d358aa612a038f88d
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 13:36:25 -0400

    aula 54 enum dentro de uma classe como atributo

---
+++ src/com/mateusborja/java2/aula54/Data.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java2.aula54;
+
+public class Data {
+
+	private int dia;
+	private int mes;
+	private int ano;
+	private DiaSemanaEnum diaSemana;
+
+	
+	
+	public Data() {
+
+	}
+	
+	
+
+	public Data(int dia, int mes, int ano, DiaSemanaEnum diaSemana) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+		this.diaSemana = diaSemana;
+	}
+
+
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public DiaSemanaEnum getDiaSemana() {
+		return diaSemana;
+	}
+
+	public void setDiaSemana(DiaSemanaEnum diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Data [dia=" + dia + ", mes=" + mes + ", ano=" + ano + ", diaSemana=" + diaSemana + "]";
+	}
+	
+	
+
+}
