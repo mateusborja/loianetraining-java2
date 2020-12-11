e93882160ba15539e2e630e3bdc97017cb4c03f0
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 13:22:06 -0400

    classe teste aula 54

---
+++ src/com/mateusborja/java2/aula54/AplicacaoDiaSemanaEnum.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java2.aula54;
+
+public class AplicacaoDiaSemanaEnum {
+	
+	public static void main(String[] args) {
+		
+		DiaSemanaEnum dia = DiaSemanaEnum.SEGUNDA;
+		
+		System.out.println(dia.toString() + " - " + dia.getValor());
+	}
+
+}
