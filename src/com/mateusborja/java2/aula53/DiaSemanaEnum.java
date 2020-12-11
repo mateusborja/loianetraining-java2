a6898a3e77eb17e1458aea9fa62405a3f5fa0dda
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 12:44:19 -0400

    Aula 53 enum

---
+++ src/com/mateusborja/java2/aula53/DiaSemanaEnum.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//Aula 53 enums e diferença entre constantes
+
+package com.mateusborja.java2.aula53;
+
+public enum DiaSemanaEnum {
+	
+	SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO
+
+}
