9bdb28779bd4ffce61a86540e2cc7848802cbf9d
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 12:38:11 -0400

    aula 53 enum e diferenca para constantes

---
+++ src/com/mateusborja/java2/aula53/DiaSemanaConstantes.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//Aula 53 enums e diferença entre constantes
+package com.mateusborja.java2.aula53;
+
+public class DiaSemanaConstantes {
+
+	public static final int SEGUNDA = 1;
+	public static final int TERCA = 2;
+	public static final int QUARTA = 3;
+	public static final int QUINTA = 4;
+	public static final int SEXTA = 5;
+	public static final int SABADO = 6;
+	public static final int DOMINGO = 7;
+
+}
