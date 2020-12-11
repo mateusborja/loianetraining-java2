13451c8f984fcd02799b4023ad5ce0ec7dd3d905
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 16:33:10 -0400

    classe teste aula 54

--- src/com/mateusborja/java2/aula54/AplicacaoFormulario.java
+++ src/com/mateusborja/java2/aula54/AplicacaoFormulario.java
@@ -3,8 +3,10 @@
  * @author Mateus Borja // UX Designer • 3D Artist • Developer
  * at www.mateusborja.life
  */
-
+//aula 54 enum classe teste
 package com.mateusborja.java2.aula54;
+
+import java.util.ArrayList;
 
 import com.mateusborja.java2.aula54.Formulario.Genero;
 
@@ -12,11 +14,23 @@ public class AplicacaoFormulario {
 	
 	public static void main(String[] args) {
 		
+		ArrayList<Formulario> listaFormularios = new ArrayList<Formulario>();
+		
 		Formulario f1 = new Formulario("Joao Honório", Genero.MASCULINO);
+		Formulario f2 = new Formulario("Leda Honório", Genero.FEMININO);
+		Formulario f3 = new Formulario("Jose Alexandre GB", Genero.MASCULINO);
+		
+		listaFormularios.add(f1);
+		listaFormularios.add(f2);
+		listaFormularios.add(f3);
+		
+		
 		
 		System.out.println(f1.getNome());
 		System.out.println(f1.getGenero());
 		System.out.println(f1);
+		
+		System.out.println(listaFormularios);
 	}
 
 }
