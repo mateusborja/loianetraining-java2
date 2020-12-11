3c77fab86ef7ae9eb5c484650ed5d68dbc648fb8
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 13:36:38 -0400

    classe teste aula 54

---
+++ src/com/mateusborja/java2/aula54/AplicacaoData.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java2.aula54;
+
+public class AplicacaoData {
+	
+	public static void main(String[] args) {
+		
+		
+		Data data = new Data(17, 2, 1978, DiaSemanaEnum.SABADO);
+		System.out.println(data);
+	}
+
+}
