986c6e6a25196e2070b2040bae022b21d2a21683
Mateus Borja <mateus.borja@gmail.com>
2020-12-14 20:46:06 -0400

    aula 56 enum com metodo abstrato

--- src/com/mateusborja/java2/aula56/Pessoa.java
+++ src/com/mateusborja/java2/aula56/Pessoa.java
@@ -3,7 +3,7 @@
  * @author Mateus Borja // UX Designer • 3D Artist • Developer
  * at www.mateusborja.life
  */
-
+//aula 56 enum com metodo abstrato
 package com.mateusborja.java2.aula56;
 
 public class Pessoa {
