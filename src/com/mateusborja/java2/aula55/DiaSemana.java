fa5073e4b7a7d7c5eb34c722307d7beddfdbdd0f
Mateus Borja <mateus.borja@gmail.com>
2020-12-15 22:28:25 -0400

    aula 55 enum metodo values e valuesOf

--- src/com/mateusborja/java2/aula55/DiaSemana.java
+++ src/com/mateusborja/java2/aula55/DiaSemana.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020.  | All rights reserved
- * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * @author Mateus Borja // UX Designer ï¿½ 3D Artist ï¿½ Developer
  * at www.mateusborja.life
  */
 //aula 55 metodos values e valuesOf
@@ -8,7 +8,7 @@ package com.mateusborja.java2.aula55;
 
 public enum DiaSemana {
 
-	SEGUNDA("Dia 1"), TERÇA("Dia 2"), QUARTA("Dia 3"), QUINTA("Dia 4"), SEXTA("Dia 5"), SÁBADO("Dia 6"),
+	SEGUNDA("Dia 1"), TERCA("Dia 2"), QUARTA("Dia 3"), QUINTA("Dia 4"), SEXTA("Dia 5"), SABADO("Dia 6"),
 	DOMINGO("Dia 7");
 
 	private String diaSemana;
