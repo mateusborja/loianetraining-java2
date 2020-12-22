d98b0d6bb8b044fbe9a872566b1b200ce2a85ff8
Mateus Borja <mateus.borja@gmail.com>
2020-12-22 15:05:36 -0400

    interface aula 64 classe anonima

---
+++ src/com/mateusborja/java2/aula64/Texto.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//aula 64 classes aninhadas - classe anonimas implementam interfaces
+package com.mateusborja.java2.aula64;
+
+public interface Texto {
+	
+	void imprimiTexto();
+
+}
