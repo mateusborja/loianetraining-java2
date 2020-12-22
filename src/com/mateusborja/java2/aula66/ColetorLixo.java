e1eb6ece094e8c209c867351faf797cd0a14b373
Mateus Borja <mateus.borja@gmail.com>
2020-12-22 15:09:38 -0400

    aula 66 garbage collector - coletor de lixo classe Runtime()

---
+++ src/com/mateusborja/java2/aula66/ColetorLixo.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//aula 65 garbage collector - coletor de lito
+package com.mateusborja.java2.aula66;
+
+import com.mateusborja.java2.aula61.Contato;
+
+public class ColetorLixo {
+
+	public static void obterMemoriaUsada() {
+
+		final int MB = 1024 * 1024;
+
+		Runtime runtime = Runtime.getRuntime(); // padrao singleton
+
+		System.out.println((runtime.totalMemory() - runtime.freeMemory()) / MB);
+
+	}
+
+	public static void main(String[] args) {
+
+		Contato[] contatos = new Contato[10000];
+
+		for (int i = 0; i < contatos.length; i++) {
+			Contato contato = new Contato("Contato" + i, "1234-56789" + i, "Contato" + i + "mail@mail.com");
+			contatos[i] = contato;
+
+		}
+
+		System.out.println("Contatos criados");
+
+		obterMemoriaUsada();
+
+		contatos = null;
+
+		Runtime.getRuntime().runFinalization();
+		Runtime.getRuntime().gc();
+
+		System.out.println("Contatos removidos da memória");
+
+		obterMemoriaUsada();
+	}
+
+}
