abc12aad71bead9a21e725a115c71ef242247169
Mateus Borja <mateus.borja@gmail.com>
2020-12-22 15:09:06 -0400

    aula 65 annotations - metadados - classe teste

---
+++ src/com/mateusborja/java2/aula65/AplicacaoAnnotation.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//aula 65 annotations - metadados - classe teste
+
+package com.mateusborja.java2.aula65;
+
+@AnnotationAula(autor = "Mateus Borja", aula = "aula 65 java 2", blog = "www.mateusborja.life")
+
+public class AplicacaoAnnotation {
+
+	public static void main(String[] args) {
+
+	}
+
+}
